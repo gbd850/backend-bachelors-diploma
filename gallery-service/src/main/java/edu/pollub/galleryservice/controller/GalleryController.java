afe0d00c8d73be408ddfb9fe10779f56d7b98a5b
Piotr Wójtowicz <46139681+gbd850@users.noreply.github.com>
2023-11-28 20:11:37 +0100

    Feature api gateway + discovery server
    * Add Api Gateway and Eureka Discovery Server
    
    * Add services as Eureka Discovery Clients
    
    * Add service names
    
    * Edit Api Gateway path configuration

--- gallery-service/src/main/java/edu/pollub/galleryservice/controller/GalleryController.java
+++ gallery-service/src/main/java/edu/pollub/galleryservice/controller/GalleryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/gallery")
+@RequestMapping("/api/gallery")
 @RequiredArgsConstructor
 @Slf4j
 public class GalleryController {
